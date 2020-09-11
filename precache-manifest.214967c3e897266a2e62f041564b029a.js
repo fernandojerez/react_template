49ff9cd6b9f157714fb1461dc24cba543947ff49
Fernando Jerez <fernandojerez@gmail.com>
2020-09-11 18:43:51 -0500

    Updates

---
+++ precache-manifest.214967c3e897266a2e62f041564b029a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ede828715e3761193f8783412abb3c4e",
+    "url": "/react_template/index.html"
+  },
+  {
+    "revision": "18b64684b58748441f9b",
+    "url": "/react_template/static/css/main.1231a5ef.chunk.css"
+  },
+  {
+    "revision": "f2d89128043da129fb51",
+    "url": "/react_template/static/js/2.9c558c69.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react_template/static/js/2.9c558c69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18b64684b58748441f9b",
+    "url": "/react_template/static/js/main.d3d0a587.chunk.js"
+  },
+  {
+    "revision": "2276bea07b9d064d6d36",
+    "url": "/react_template/static/js/runtime-main.2621eb7c.js"
+  }
+]);
